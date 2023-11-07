fb662248d69ccc4a47687cff03a47f3f8dcfb7cb
tunnerfrank <123299776+tunnerfrank@users.noreply.github.com>
2023-11-07 11:35:37 +0300

    Add files via upload

---
+++ roadmaptoyourcoochie.py
@@ -0,0 +1,37 @@
+#ask the user which relationshiplovelanguage they want to go for a start
+#create a second condtion for which the user will choose gift or touch
+#create a third condition for which the user has to choose red flags to consider
+#create a forth condtion to choose money or love
+#finals won you are eating coochie
+print("Welcome to ROADMAP-TO-YOUR-COOCHIE CHAMPIONSHIP")
+round1 = input("Question1: What is your girlfriends love language \n A: HUGS B: CUDDLES C: AFFIRMATIONS")
+if round1 == "CUDDLES" or round1 == "CUDDLES".lower() :
+    print("CONGRATULATIONS YOU MOVE TO THE NEXT ROUND OF COOCHIE HUNTING")
+if round1 == "HUGS" or round1 == "HUGS".lower:
+    print("Go hug yourself into the friendzone")
+    breakpoint
+if round1 == "AFFIRMATIONS" or round1 == "AFFIRMATIONS".lower:
+    print("enda jiongeleshe vile utapata pesa")
+    breakpoint
+round2 = input("Question2: What does she prefer more?\n A: TOUCH B: GIFTS")
+if round2 == "GIFTS".lower() or round2 == "GIFTS" :
+    print("Very nice I see you cumming very soon")
+    print ("Welcome to round 3")
+if round2 == "TOUCH".lower() or round2 == "TOUCH" :
+    print ("Go touch yourself!!")
+    breakpoint
+round3 = input ("What are her red flags in guys.")
+print (f"{round3}")
+agree = input ("does she agreee?")
+if agree == "YES":
+    print ("Wow , you are actually a simp")
+    breakpoint
+else :
+    print("maybe be next time you get to actually nut in some coochie")
+    breakpoint
+round4 = input("You are definetly now getting some. \n FINALLY WHO IS SHE \n MONEY OR LOVE")
+if round4 == "LOVE" or round4 == "LOVE".lower():
+    print("eat that coochie DOG. YOU DESERVE IT YOU SIMP")
+else :
+    print ("MAYBE NEXT TIME")
+    
